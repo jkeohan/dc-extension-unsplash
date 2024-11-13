4cfd02eab5c0e9057358b49d8fbf77a8131c215b
jkeohan <jkeohan@gmail.com>
2024-11-13 06:54:02 -0500

    adding useSdk hook

---
+++ src/useSdk.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import { init } from 'dc-extensions-sdk';
+
+export const useSdk = () => {
+	const [sdk, setSdk] = useState({});
+	useEffect(() => {
+		(async () => {
+			const s = await init();
+			console.log('after await init()');
+			s.frame.startAutoResizer();
+			const values = await s.field.getValue();
+			console.log('values', values);
+			const apiKey = s.params.installation?.api_key;
+			const payload = {
+				sdk: s,
+				url: values.url,
+				alt_description: values.alt_description,
+				apiKey,
+			};
+
+			setSdk(payload);
+		})();
+	}, []);
+
+	return sdk;
+};
